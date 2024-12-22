0dccb041d9a23ccfe6419b5885ccf25d9e6bfb6f
vinokurov07 <vinokurovv07@mail.ru>
2024-12-22 15:46:05 +0700

    First commit

---
+++ game.py
@@ -0,0 +1,15 @@
+def func(a,b) -> int:
+    """_summary_
+
+    Args:
+        int (_type_): _description_
+
+    Returns:
+        int: _description_
+    """
+
+    print(f'это {a*b}')
+    return a*b
+
+func(5,10)
+
