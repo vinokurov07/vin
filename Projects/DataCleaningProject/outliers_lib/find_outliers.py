4648d68866a449cff388fd30397f93982e7dd083
vinokurov07 <vinokurov07@mail.ru>
2026-03-08 22:10:24 +0700

    add find_outliers_quantile

--- Projects/DataCleaningProject/outliers_lib/find_outliers.py
+++ Projects/DataCleaningProject/outliers_lib/find_outliers.py
@@ -61,3 +61,11 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
 
 def test_func(a,b):
     return a+b
+
+def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    x = data[feature]
+    lower_bound = x.quantile(left)
+    upper_bound = x.quantile(right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
